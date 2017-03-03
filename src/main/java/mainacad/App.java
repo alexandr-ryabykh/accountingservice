ffc501503e9b009e231d6e7f241933a4114271d3
Khristophorov <zaq1@WSX>
2017-03-03 20:02:13 +0200

    Adopted for test run

--- src/main/java/mainacad/App.java
+++ src/main/java/mainacad/App.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
-        System.out.println();
-        System.out.println();
+        SpringApplication.run(App.class, args);         
     }
 
 }
